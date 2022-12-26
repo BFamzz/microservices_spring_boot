6dc183e4d4b0241c65805d3f826488d57dfdc0a4
Busayo Famutimi <famutimi.busayo@gmail.com>
2022-12-26 11:03:13 +0100

    Added docker and docker-compose with OpenAPIdocumentation

--- api/src/main/java/com/microservices/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/microservices/api/composite/product/ProductCompositeService.java
@@ -3,8 +3,21 @@ package com.microservices.api.composite.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Composite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
     
+    @Operation(summary = "${api.composite.get-composite.description}", description = "${api.composite.get-composite.notes}")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @GetMapping(value = "/products-composite/{productId}", produces = "application/json")
     ProductAggregate getProduct(@PathVariable int productId);
 }
--- util/src/main/java/com/microservices/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/com/microservices/util/http/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.microservices.util.http;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.microservices.api.exceptions.BadRequestException;
 import com.microservices.api.exceptions.InvalidInputException;
 import com.microservices.api.exceptions.NotFoundException;
 
@@ -18,6 +21,14 @@ import com.microservices.api.exceptions.NotFoundException;
 class GlobalControllerExceptionHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public @ResponseBody HttpErrorInfo handleBadRequestExceptions(
+        ServerHttpRequest request, NotFoundException ex) {
+
+        return createHttpErrorInfo(BAD_REQUEST, request, ex);
+    }
 
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
